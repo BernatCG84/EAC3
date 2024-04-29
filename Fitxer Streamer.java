d38d208595f461202f211727b59e0c3585b50c04
BernatCG84 <90278775+BernatCG84@users.noreply.github.com>
2024-04-29 19:39:33 +0200

    Create Fitxer Streamer.java

---
+++ Fitxer Streamer.java
@@ -0,0 +1,20 @@
+// Fitxer Streamer.java
+
+public class Streamer {
+	private String plataforma;
+	private float sou;
+
+	public void setPlataforma (String plataforma) {
+		this.plataforma=plataforma;
+	}
+	public String getPlataforma () {
+		return plataforma;
+	}
+	
+	public void setSou (float sou) {
+		this.sou=sou;
+	}
+	
+	public float getSou () {
+		return sou;
+	}
